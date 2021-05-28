47178cddd707a1da20198c7e563dde6b61fc2052
pluto <337058558@qq.com>
2021-05-28 11:34:02 +0800

    权限库

---
+++ library/src/main/java/com/pluto/jan/library/PermissionX.kt
@@ -0,0 +1,31 @@
+package com.pluto.jan.library
+
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * Create by Pluto-Jan
+ * on 2021/5/28
+ * PermissionX
+ */
+object PermissionX {
+
+    private const val TAG = "InvisibleFragment"
+
+    fun request(
+        activity: FragmentActivity,
+        vararg permissions: String,
+        callback: PermissionCallback
+    ) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
+    }
+
+}
